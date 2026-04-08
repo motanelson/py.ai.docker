c20adebb9a7c1dcdf824962a02a6843fdb476d76
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-04-08 21:28:44 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-printf "\ec\e[40;37m\n"
-docker build -t hello-world-docker .
-docker create --name hello-world-docker hello-world-docker
-docker start hello-world-docker
-docker cp hello-world-docker:/hello.txt hello.txt
-docker rm hello-world-docker
-cat hello.txt
- 
